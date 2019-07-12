6af5a56a0d2da9a4d5f7664bee7cca1ea7e9c4b6
amilkar miguel <amcmiki@gmail.com>
2019-07-12 00:25:22 -0400

    servicio login y registro usuarios

---
+++ routes/api/v1.0/services.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const mogoose =require('mongoose')
+const connect =  require('../../../database/collections/connect')
+const Registro = require('../../../database/collections/users')
+
+const Img = require('../../../database/collections/img')
+const express = require('express')
+
+
+//esta variables toma el valor de la IP
+const HOST = require('../../../database/collections/HOST')
+
+//////////////////////// multer para imagenes
+const multer = require('multer');
+
+const route = express.Router()
+
+// metodos de peticion GET, POTS, PUT, DELETE
+
+route.get('/', (req, res) =>{
+    res.send({ menssage:'SERVICIO API-RES TIENDA MOVIL'})
+})
+
+
+route.get('/list/:email', (req, res) =>{
+    //res.send({ email:`${req.params.email}`,password:`${req.params.pass}`})
+    console.log(req.params)
+    let email =req.params.email
+
+    Registro.find({"email":email}, (err, user) =>{
+        if(err) return res.status(500).send({menssage:`Error en la peticion: ${err}`})
+        if(!user) return res.status(404).send({message:`usuario no existe`})
+
+        res.status(200).send({'email':user})
+    })
+})
+
+route.get('/login/:email=:password', (req, res) =>{
+    //res.send({ email:`${req.params.email}`,password:`${req.params.pass}`})
+    console.log(req.params)
+
+    let email =req.params.email
+    let password=req.params.password
+
+    Registro.find({"email":email,"password":password}, (err, user) =>{
+        if(err) return res.status(500).send({menssage:`Error en la peticion: ${err}`})
+        if(user.length == 0) return res.status(404).send({message:`usuario no existe`})
+
+        res.status(200).send({'email':user})
+    })
+})
+
+//registro de usuarios
+route.post('/registro', (req, res) =>{
+    console.log('POST /api/registro')
+    console.log(req.body)
+
+    let registro = new Registro()
+    registro.name =req.body.name
+    registro.lastname = req.body.lastname
+    registro.phone = req.body.phone
+    registro.email = req.body.email
+    registro.password = req.body.password
+
+
+
+
+    Registro.findOne({'email':registro.email},(err,e)=>{
+        if(e){
+            console.log('email repetido')
+            res.status(404).send({message:`Este email ${registro.email} ya se encuentra registrado`})
+        }
+        else{
+            registro.save((err, usertStored) =>{
+                if(err) {
+                  res.status(404).send({messaje: `Error al salvar la base de datos:${err}`})
+                 console.log(err)
+                }
+                res.status(200).send(usertStored)
+            })
+        }
+
+        //res.status(404).send
+
+    })
+/* /////////////////
+    registro.save((err, usertStored) =>{
+        if(err) res.status(500).send({messaje: `Error al savar la base de datos:${err}`})
+
+        //res.status(200).send({usertStored})
+    })
+ *///////
+})
+
+
+module.exports = route
